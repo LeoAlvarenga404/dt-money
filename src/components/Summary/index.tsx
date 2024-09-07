5a87a3273b71fae2aa5e597acded8935dadbb783
leonardo <leonardo.palvarenga@gmail.com>
2024-09-07 17:10:21 -0300

    fix: a merda q eu fiz

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,29 +1,35 @@
 import { SummaryCard, SummaryContainer } from "./styles";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { useSummary } from "../../hooks/useSummary";
+import { priceFormatter } from "../../utils/formatter";
 
 export function Summary() {
+  const summary = useSummary()
+
+  
+
   return (
-    <SummaryContainer>
+    <SummaryContainer>     
       <SummaryCard>
         <header>
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#F75a68" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
